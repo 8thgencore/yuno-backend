88c84e704ca7f1f41e75a16bb9f18ba06f4d9873
8thgencore <axelmaker@ya.ru>
2023-01-19 13:47:11 +0000

    ⚒️Add password for Redis

--- src/app/api/deps.py
+++ src/app/api/deps.py
@@ -26,8 +26,12 @@ reusable_oauth2 = OAuth2PasswordBearer(
 
 
 async def get_redis_client() -> Redis:
+    redis = await aioredis.from_url(
+    "redis://localhost", username="user", password="sEcRet"
+)
     redis = aioredis.from_url(
         f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+        password=settings.REDIS_PASSWORD,
         max_connections=10,
         encoding="utf8",
         decode_responses=True,
